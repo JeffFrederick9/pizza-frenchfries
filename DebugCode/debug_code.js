beeea215d0aa5fad45dd7faeec39b92adc7d1f01
jefffrederick9 <jefffrederick9@gmail.com>
2025-01-15 14:08:56 -0500

    More Debug

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,10 +3,14 @@
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let num3 = parseInt(document.getElementById('input3').value);
+    let num4 = parseInt(document.getElementById('input4').value);
+    
+    
     // Check if inputs are valid numbers
-    if (!isNaN(num1) && !isNaN(num2)) {
+    if (!isNaN(num1) && !isNaN(num2)&& !isNaN(num3)&& !isNaN(num4)) {
     // Perform the operation
-                    let result = multiply(num1, num2);
+                    let result = multiply(num1, num2, num3, num4);
 
                     // Display the result
                     displayResult(result);
@@ -15,12 +19,12 @@
                 }
             }
 
-            function multiply(a, b) {
+            function multiply(a, b ,c, d) {
                 // Introduce a debugger statement to pause execution
                 debugger;
 
                 // Multiply the numbers
-                return a * b;
+                return ((a * b) + c)/d;
             }
 
             function displayResult(result) {
@@ -29,4 +33,3 @@
                 resultElement.textContent = `The result is: ${result}`;
             }
         
-            
